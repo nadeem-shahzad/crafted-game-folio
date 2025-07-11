43532ab2383262595f6cf6f3670d21863e257672
Nadeem Shahzad <nadeem.shahzad@nplays.tech>
2025-07-11 10:01:33 +0500

    -- Upwork Logo Updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,15 @@
 
 import { Button } from "@/components/ui/button";
-import { Download, Github, Linkedin, Facebook, Instagram, Upwork } from "lucide-react";
+import { Download, Github, Linkedin, Facebook, Instagram } from "lucide-react";
 import ScrollIndicator from "./ScrollIndicator";
+
+const UpworkIcon = () => (
+    <svg fill="#ffffff" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" width="20" height="20">
+      <g>
+        <path d="M24.75 17.542c-1.469 0-2.849-0.62-4.099-1.635l0.302-1.432 0.010-0.057c0.276-1.521 1.13-4.078 3.786-4.078 1.99 0 3.604 1.615 3.604 3.604 0 1.984-1.615 3.599-3.604 3.599zM24.75 6.693c-3.385 0-6.016 2.198-7.083 5.818-1.625-2.443-2.865-5.38-3.583-7.854h-3.646v9.484c-0.005 1.875-1.521 3.391-3.396 3.396-1.875-0.005-3.391-1.526-3.396-3.396v-9.484h-3.646v9.484c0 3.885 3.161 7.068 7.042 7.068 3.885 0 7.042-3.182 7.042-7.068v-1.589c0.708 1.474 1.578 2.974 2.635 4.297l-2.234 10.495h3.729l1.62-7.615c1.417 0.906 3.047 1.479 4.917 1.479 4 0 7.25-3.271 7.25-7.266 0-4-3.25-7.25-7.25-7.25z"></path>
+      </g>
+    </svg>
+)
 
 const Hero = () => {
   const scrollToProjects = () => {
@@ -69,7 +77,7 @@ const Hero = () => {
                 <Linkedin className="h-5 w-5" />
               </Button>
               <Button variant="outline" size="icon" className="rounded-full" onClick={openUpwork}>
-                <Upwork className="h-5 w-5" />
+                <UpworkIcon />
               </Button>
               <Button variant="outline" size="icon" className="rounded-full" onClick={openFacebook}>
                 <Facebook className="h-5 w-5" />
